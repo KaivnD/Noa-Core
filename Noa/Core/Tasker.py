29a40729581fbf6421d6e0ba2e1da876f15ebbab
KaivnD <KaivnD@hotmail.com>
2019-08-06 23:32:09 +0800

    init:初始化

---
+++ Noa/Core/Tasker.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+class Tasker (ABC):
+    @abstractmethod
+    def input (self):
+        pass
+    
+    @abstractmethod
+    def process (self):
+        pass
+
+    @abstractmethod
+    def output (self):
+        pass
+
+class ApiTasker (Tasker):
+    def input(self):
+        print('input')
+
+    def process(self):
+        print('process')
+
+    def output(self):
+        print('output')
