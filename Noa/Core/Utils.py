cd409325e6301ddd461b6b29cb24fbfa6501b012
clef <susunhao@163.com>
2019-08-28 00:40:24 +0800

    feat:add rhino and txt output

--- Noa/Core/Utils.py
+++ Noa/Core/Utils.py
@@ -1,31 +1,88 @@
 from Noa.Hooks import Hook,before,after
 from lxml import etree
-import math
+import math,rhino3dm,json
 
 class Utils(object):
     def __init__(self,import_data,output_data):
         self.import_data=import_data
         self.output_data=output_data
     
-    def output_file(self,content,file_name,cover=True):
+    def run(self):
+        #入口方法
+        return(self.process())
+
+    @before('txt_output')
+    def txt_pre_deal(self,content,file_name='test',cover=True):
+        #预处理数据，用来重写。
+        pass
+
+    @Hook
+    def txt_output(self,content,file_name='test',cover=True):
+    #将数据储存为txt
         g=lambda: 'w' if cover else 'a'
-        with open(file_name,g()) as f:
-            f.write(str(content))
+        try:
+            with open('{}.txt'.format(file_name),g()) as f:
+                f.write(str(content))
+        except UnicodeEncodeError:
+            with open('{}.txt'.format(file_name),g(),encoding='utf-8') as f:
+                f.write(str(content))
         return(True)
 
+    @before('rhino_output')
+    def rhino_pre_deal(self,content,file_name='test',cover=True):
+        #预处理数据，用来重写。
+        pass
+
+    @Hook
+    def rhino_output(self,content,file_name='test',cover=True):
+    #将数据储存为rhino文件
+        self.model.Write(file_name+'.3dm')
+        return(True)
+
+    @before('svg_output')
+    def svg_pre_deal(self,content,file_name='test',cover=True):
+        #预处理数据，用来重写。
+        pass
+
+    @Hook
+    def svg_output(self,content,file_name='test',cover=True):
+    #将数据储存为svg文件
+        pass
+        return(True)
+
+    @before('dxf_output')
+    def dxf_pre_deal(self,content,file_name='test',cover=True):
+        #预处理数据，用来重写。
+        pass
+
+    @Hook
+    def dxf_output(self,content,file_name='test',cover=True):
+        #将数据储存为svg文件
+        pass
+        return(True)
+    
+    def output_file(self,content,file_name,cover=True):
+        #输出方法，建议在output方法中调用该方法。
+        file_type=file_name.split('.')[1]
+        method_dict={'txt':self.txt_output,
+                     '3dm':self.rhino_output,
+                     'svg':self.svg_output,
+                     'dxf':self.dxf_output}
+        result=method_dict[file_type](content,file_name.split('.')[0])
+        return(result)
 
 class OsmConvert(Utils):
     #将osm格式的文件解析为json。
 
     @before('process')
     def input(self):
+        #输入方法，将经纬度处理为XY坐标体系。
         scale=self.import_data['scale']
         html=etree.parse(self.import_data['file_name'])
         self.minlat = float(str(html.xpath('//bounds/@minlat')[0]))
         self.maxlat = float(str(html.xpath('//bounds/@maxlat')[0]))
         self.minlon = float(str(html.xpath('//bounds/@minlon')[0]))
         self.maxlon = float(str(html.xpath('//bounds/@maxlon')[0]))
-        print(self.minlat)
         a = self.maxlat - self.minlat
         b = self.minlat + a / 2.0
         self.c = 3.1415926535897931 * 6371000 / 180.0 * scale
@@ -35,8 +92,6 @@ class OsmConvert(Utils):
         X = (lon - self.minlon) * self.d
         Y = (lat - self.minlat) * self.c
         return((X,Y))
-
-
 
     @Hook
     def process(self):
@@ -69,11 +124,75 @@ class OsmConvert(Utils):
                     key=children_tag.xpath('./@k')[0]
                     value=children_tag.xpath('./@v')[0]
                     way_dict[way_id]['info'][key]=value
-        print(way_dict)
-        info_dict=dict(node_info=node_dict,way_info=way_dict)
-        return (info_dict)
+        
+        self.info_dict=dict(node_info=node_dict,way_info=way_dict)
+        return (self.info_dict)
 
 
-    @before('process')
+    @after('process')
     def output(self):
+        self.output_file(self.info_dict,self.output_data['file_name'])
         pass
+    
+    @before('txt_output')
+    def txt_pre_deal(self,content,file_name='test',cover=True):
+        self.info_dict=json.dumps(self.info_dict)
+    
+    @before('rhino_output')
+    def rhino_pre_deal(self,content,file_name='test',cover=True):
+        tag_list=[  "aerialway",
+                    "aeroway",
+                    "amenity",
+                    "barrier",
+                    "boundary",
+                    "building",
+                    "craft",
+                    "emergency",
+                    "geological",
+                    "highway",
+                    "historic",
+                    "landuse",
+                    "leisure",
+                    "man_made",
+                    "military",
+                    "natural",
+                    "office",
+                    "place",
+                    "power",
+                    "public_transport",
+                    "railway",
+                    "route",
+                    "shop",
+                    "sport",
+                    "tourism",
+                    "waterway"]
+        layer_list=[]
+
+        self.model=rhino3dm.File3dm()
+        objects=self.model.Objects
+        layers=self.model.Layers
+
+
+
+        for way in self.info_dict['way_info']:
+            #'32881263': {'nodes': [(146.25242773565972, 62.26915892096578), (172.27842306686892, 54.90805477719295), (194.73222295988276, 37.82851404488768), (202.70695466303127, 30.800994680996393), (204.4973770742999, 28.4770207138299)], 'info': {'highway': 'secondary', 'name': '上海路', 'oneway': 'yes'}}
+            temp=self.info_dict['way_info'][way]
+            for i in temp['info']:
+                if i in tag_list:
+                    layer_name='{}${}'.format(i,temp['info'][i])
+                    if layer_name not in layer_list:
+                        layer_list.append(layer_name)
+                        tem_layer=rhino3dm.Layer()
+                        tem_layer.Name = layer_name
+                        layers.Add(tem_layer)
+                    break
+            attr=rhino3dm.ObjectAttributes()
+            attr.LayerIndex = layers.FindName(layer_name,None).Index
+            
+            line = rhino3dm.Polyline()
+            for node in temp['nodes']:
+                line.Add(node[0],node[1],0)
+            print(layers.FindName(layer_name,None).Index)
+            objects.AddPolyline(line,attr)
+        return(True)
+
--- Osm_ana_test.py
+++ Osm_ana_test.py
@@ -1,5 +1,5 @@
 from Noa.Core.Utils import *
 import_data={'file_name':'temp/test.osm','scale':1}
-output_data={'file_name':'test.txt'}
+output_data={'file_name':'test.3dm'}
 a=OsmConvert(import_data,output_data)
-a.process()
+a.run()
